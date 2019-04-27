9c915605346071a1fbd0e3686cefb3bac6da2da3
a972667237 <wuezmonster@gmail.com>
2019-04-27 14:27:13 +0800

    test: add test for Answer generator

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,35 @@
 package tw.core.generator;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.Before;
+import org.junit.Test;
+import tw.GuessNumberModule;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+    @Inject
+    private AnswerGenerator answerGenerator;
 
+    @Before
+    public void setUp() {
+        Guice.createInjector(new GuessNumberModule())
+                .injectMembers(this);
+    }
+
+    @Test
+    public void generateTest() throws OutOfRangeAnswerException {
+        assertDoesNotThrow(() -> answerGenerator.generate());
+        assertTrue(answerGenerator.generate() instanceof Answer);
+    }
 }
 
