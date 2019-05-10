0f1d07dd06a3dbb19e5e4f3fa73552c4076a6d47
a972667237 <wuezmonster@gmail.com>
2019-05-11 00:12:17 +0800

    test: add input validator test

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,27 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    private InputValidator valid = new InputValidator();
+
+    @Test
+    public void TestValidate() {
+        String RepeatNumString = "1 1 2 3";
+        String LackNumString = "1 2 3";
+        String CorrectNumString = "1 2 3 4";
+        String InputErrorString = "A";
+        assertFalse(valid.validate(RepeatNumString));
+        assertFalse(valid.validate(LackNumString));
+        assertFalse(valid.validate(InputErrorString));
+        assertTrue(valid.validate(CorrectNumString));
+    }
 
 }
