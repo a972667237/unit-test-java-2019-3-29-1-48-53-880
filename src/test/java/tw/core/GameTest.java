a840c28ecf1428d90e35bf79cdcc297d8cf40723
a972667237 <wuezmonster@gmail.com>
2019-05-10 23:43:03 +0800

    test: add game test

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,79 @@
 package tw.core;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.sun.source.tree.AssertTree;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import tw.GuessNumberModule;
+import tw.core.generator.RandomIntGenerator;
+import tw.core.model.GuessResult;
+
+import javax.inject.Inject;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static java.util.stream.IntStream.range;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static tw.core.GameStatus.CONTINUE;
+import static tw.core.GameStatus.FAIL;
+import static tw.core.GameStatus.SUCCESS;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
 
+// TODO: using Jukito?
 public class GameTest {
+    String gameRandomString = new RandomIntGenerator().generateNums(9, 4);
+
+    @Inject
+    private Game game;
+
+    @Before
+    public void setUp() {
+        Guice.createInjector(new GuessNumberModule(),
+                new AbstractModule() {
+                    @Override
+                    protected void configure() {
+                        bind(RandomIntGenerator.class).toInstance(new RandomIntGenerator(){
+                            @Override
+                            public String generateNums(Integer digitmax, Integer numbersOfNeed) {
+                                return gameRandomString;
+                            }
+                        });
+                    }
+                })
+                .injectMembers(this);
+    }
+
+    @Test
+    public void TestGuess() {
+        // test guess result append into tail
+        GuessResult append_result = game.guess(Answer.createAnswer(gameRandomString));
+        assertTrue(append_result.equals(game.guessHistory().get(0)));
+        assertTrue(append_result instanceof GuessResult);
+    }
+
+    @Test
+    public void TestCheckStatus() {
+        StringBuffer gameRandomStringBuffer = new StringBuffer(gameRandomString);
+        char firstNum = gameRandomStringBuffer.charAt(0);
+        char secondNum = gameRandomStringBuffer.charAt(2);
+        String wrongString = gameRandomString.replace(secondNum, firstNum).replace(firstNum, secondNum);
+        game.guess(Answer.createAnswer(wrongString));
+        assertTrue(game.checkStatus() == CONTINUE);
+        game.guess(Answer.createAnswer(gameRandomString));
+        assertTrue(game.checkStatus() == SUCCESS);
+
+    }
 
 }
